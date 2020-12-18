d76772376cd4fecb1259c78567bf0e82d3e74c40
cursednat <74876759+cursednat@users.noreply.github.com>
2020-12-18 17:05:12 +0300

    Add files via upload

---
+++ lab5.py
@@ -0,0 +1,46 @@
+class FPN:
+# точность дробной части
+    exponent = -6 
+    
+    def number(self):
+        pass
+    
+    def __init__(self, number=0):
+        self.number = int(number * 10 ** -FPN.exponent)
+        
+    def __add__(self, other):
+        result = FPN()
+        result.number = self.number + other.number
+        return result
+         
+    def __sub__(self, other):
+        result = FPN()
+        result.number = self.number - other.number
+        return result
+
+#умножение        
+    def __mul__(self, other):
+        result = FPN()
+        result.number = self.number * other.number // 10 ** -FPN.exponent
+        return result
+ 
+#деление        
+    def __truediv__(self, other):
+        result = FPN()
+        result.number = int(self.number / other.number * 10 ** -FPN.exponent)
+        return result
+    
+    def __str__(self):
+        num_str = str(self.number)
+        sign = '-' if num_str[0] == '-' else ''
+        index = 1 if sign else 0
+        int_part = num_str[index:FPN.exponent]
+        return f"{sign}{int_part if int_part else 0}.{num_str[FPN.exponent:]}"
+
+f = FPN(10)
+g = FPN(1.23)
+print(f+g)
+print(f-g)
+print(f*g)
+print(f/g)
+
